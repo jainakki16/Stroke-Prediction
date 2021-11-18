ddab082e959251b1b1ee3aacccece325132d7bec
jainakki16 <akshatjain981000@gmail.com>
2021-11-19 01:52:43 +0530

    updated web application

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-#importing libraries
 import os
 import numpy as np
 import flask
@@ -6,17 +5,9 @@ import pickle
 from flask import Flask, render_template, request
 
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, template_folder='templates')
 app.config["DEBUG"] = True
 
-
-
-
-
-#creating instance of the class
-
-
-#to tell flask what url shoud trigger the function index()
 @app.route('/')
 @app.route('/index')
 def index():
@@ -24,18 +15,24 @@ def index():
 
 
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,9)
-    loaded_model = pickle.load(open("model_pkl","rb"))
+    to_predict = np.array(to_predict_list).reshape(1,10)
+    loaded_model = pickle.load(open("model_final_pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
 
 
-@app.route('/result',methods = ['POST'])
+@app.route('/result', methods = ['POST'])
 def result():
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
+        
+        to_predict_list['age'] = int(to_predict_list['age'])
+        to_predict_list['hypert'] = int(to_predict_list['hypert'])
+        to_predict_list['heart_d'] = int(to_predict_list['heart_d'])
+        to_predict_list['avg_gl'] = int(to_predict_list['avg_gl'])
+        to_predict_list['c_loss'] = int(to_predict_list['c_loss'])
+
         to_predict_list=list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
         result = ValuePredictor(to_predict_list)
         
         if int(result)==1:
@@ -44,7 +41,5 @@ def result():
             prediction='Not a chance of getting stroke for now'
             
         return render_template("result.html",prediction=to_predict_list)
-    
-    
 
 app.run()
