a98a607b7554970977f8fcf72c2cda7dd3a6e177
AVIN111 <avinash.patil18@st.niituniversity.in>
2021-12-19 17:53:01 +0530

    Updated unit test cases

--- pybuilder/src/unittest/python/unittest_tests.py
+++ pybuilder/src/unittest/python/unittest_tests.py
@@ -8,15 +8,15 @@ class SimpleTest(unittest.TestCase):
     def testcase1(self):
         case_1 = {}
         case_1['gender'] = 'Male'
-        case_1['age'] = 45
-        case_1['hypertension'] = '0'
+        case_1['age'] = 25
+        case_1['hypertension'] = '1'
         case_1['heart_disease'] = '1'
         case_1['marital_status'] = 'No'
-        case_1['work_type'] = 'Private'
-        case_1['residence_type'] = 'Urban'
-        case_1['avg_glucose_level'] = 51
-        case_1['bmi'] = 6
-        case_1['smoking_status'] = 'smokes'
+        case_1['work_type'] = 'Never_worked'
+        case_1['residence_type'] = 'Rural'
+        case_1['avg_glucose_level'] = 175
+        case_1['bmi'] = 18
+        case_1['smoking_status'] = 'never smoked'
 
         self.assertEquals(predict_stroke(case_1), "Congratulations! Based on your inputs you are not likely to get a stroke.")
 
@@ -24,14 +24,30 @@ class SimpleTest(unittest.TestCase):
     def testcase2(self):
         case_2 = {}
         case_2['gender'] = 'Female'
-        case_2['age'] = 34
-        case_2['hypertension'] = '0'
+        case_2['age'] = 30
+        case_2['hypertension'] = '1'
+        case_2['heart_disease'] = '1'
+        case_2['marital_status'] = 'No'
+        case_2['work_type'] = 'Never_worked'
+        case_2['residence_type'] = 'Rural'
+        case_2['avg_glucose_level'] = 120
+        case_2['bmi'] = 25
+        case_2['smoking_status'] = 'never smoked'
+
+        self.assertEquals(predict_stroke(case_2), "Congratulations! Based on your inputs you are not likely to get a stroke.")
+
+
+    def testcase3(self):
+        case_2 = {}
+        case_2['gender'] = 'Female'
+        case_2['age'] = 40
+        case_2['hypertension'] = '1'
         case_2['heart_disease'] = '0'
         case_2['marital_status'] = 'Yes'
         case_2['work_type'] = 'children'
         case_2['residence_type'] = 'Rural'
-        case_2['avg_glucose_level'] = 140
-        case_2['bmi'] = 23
-        case_2['smoking_status'] = 'never smoked'
+        case_2['avg_glucose_level'] = 190
+        case_2['bmi'] = 10
+        case_2['smoking_status'] = 'formerly smoked'
 
-        self.assertEquals(predict_stroke(case_2), "Congratulations! Based on your inputs you are not likely to get a stroke.")
+        self.assertEquals(predict_stroke(case_2), "Please consult a doctor. Based on your inputs you are likely to get stroke.")
